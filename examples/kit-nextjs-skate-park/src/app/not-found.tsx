2d8950a10bee0af3a46a5bb0744d645a1ea49906
nihad.keric <nihad.keric@zurich.ch>
2026-03-23 11:05:35 +0100

    Add EventInfo component and update component map
    - Introduced a new EventInfo component for displaying event details.
    - Updated the component map to include the new EventInfo component.
    - Modified the package-lock.json to add peer dependencies for various packages.
    - Removed the "Page not found" heading from the NotFound component for a cleaner look.
    - Added a new file to store sensitive authentication credentials (auth-secrt).

--- examples/kit-nextjs-skate-park/src/app/not-found.tsx
+++ examples/kit-nextjs-skate-park/src/app/not-found.tsx
@@ -23,7 +23,6 @@ export default async function NotFound() {
 
   return (
     <div style={{ padding: 10 }}>
-      <h1>Page not found</h1>
       <p>This page does not exist.</p>
       <Link href="/">Go to the Home page</Link>
     </div>
